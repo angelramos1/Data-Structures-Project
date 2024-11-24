9e0dcb4e1f099fc6ae1c1e43aa8e497139636629
angelramos1 <angel.ramos48@upr.edu>
2024-11-24 19:47:58 -0400

    se completó la clase cliente

--- Cliente.java
+++ Cliente.java
@@ -1,3 +1,29 @@
+//Esta clase representa un cliente que reserva asientos en el sistema.
+//Se utiliza los atributos para almacenar información del cliente.
 public class Cliente {
-    
+    private String name;
+    private String email;
+    private String phoneNumber;
+//Constructor 
+    public Cliente(String name, String email, String phoneNumber){
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+//Método para llamar al nombre.
+    public String getName(){
+        return name;
+    }
+//Método para llamar el email.
+    public String getEmail(){
+        return email;
+    }
+//Método para llamar su teléfono.
+    public String getPhoneNumber(){
+        return phoneNumber;
+    }
+//Método para devolver en un string la información del cliente.
+    public String toString() {
+        return "Name: " + name + ", Email: " + email + ", Phone: " + phoneNumber;
+    }
 }
